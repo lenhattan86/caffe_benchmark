6b0008ac3f1dfd645f23f2e997717c5da5f74c44
Nhat Tan <nhatle@paypal.com>
2019-08-12 12:38:25 -0700

    update

--- examples/mnist/train_lenet_cpu.sh
+++ examples/mnist/train_lenet_cpu.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 set -e
 
-SECONDS=0
-./build/tools/caffe train --solver=examples/mnist/lenet_solver_cpu.prototxt $@
-duration=$SECONDS
-echo "$duration seconds elapsed."
+start=$(date +%Y%m%d%H%M%S);
+caffe train --solver=examples/mnist/lenet_solver_cpu.prototxt $@
+end=$(date +%Y%m%d%H%M%S);
+elapsed=$(($end-$start));
+echo "============================="
+echo "$elapsed seconds"
