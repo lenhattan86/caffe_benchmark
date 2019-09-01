2d6a9ba6d192da5e368c22a4a518eafc9ee16668
Tan N. Le <lenhattan86@gmail.com>
2019-09-01 09:17:12 -0400

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,29 @@
-model=$1
-echo "run model $model"
+#!/bin/bash
+if [ -z "$1" ]
+then
+	model="lenet"
+else
+	model="$1"
+fi
+
+if [ -z "$2" ]
+then
+	device="cpu"
+else
+	device="$2"
+fi
+
+SECONDS=0
+if [ $model == "lenet" ]; then
+    if [ $device == "cpu" ]; then
+        caffe train --solver=examples/mnist/lenet_solver_cpu.prototxt $@
+    elif [ $device == "gpu" ]; then
+        caffe train --solver=examples/mnist/lenet_solver.prototxt $@    
+    else
+        echo "unknown device"        
+    fi  
+else
+    echo "$model does not exist"    
+fi
+duration=$SECONDS
+echo "$duration seconds elapsed."
